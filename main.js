0eddf868b75aa88254dbf229ab5d06642f76dd5a
mossa <martin.ossa.8@gmail.com>
2023-02-15 14:38:32 +0100

    Fix add book function

--- main.js
+++ main.js
@@ -2,24 +2,13 @@
 /* eslint-disable no-unused-vars */
 import Book from './modules/Book.js';
 
-const booksList = document.getElementById('booksList');
-const newBook = document.getElementById('newBook');
-const contactInfo = document.getElementById('contactInfo');
-const recordSec = document.querySelector('.record-section');
 const addForm = document.querySelector('.form');
-const contactSec = document.querySelector('.contact');
 const showBooks = document.querySelector('.record');
 const Title = document.querySelector('.title');
 const Author = document.querySelector('.author');
 const btn = document.querySelector('.form button');
 
 const bookc = new Book();
-
-
-newBook.addEventListener('click', () => {
-  recordSec.style.display = 'block';
-  addForm.style.display = 'block';
-});
 
 
 btn.addEventListener('click', () => {
@@ -42,7 +31,6 @@ window.remove = (id) => {
 };
 
 // Local storage
-
 window.addEventListener('DOMContentLoaded', () => {
   const books = JSON.parse(localStorage.getItem('data')) === null ? [] : JSON.parse(localStorage.getItem('data'));
   books.forEach((i) => {
