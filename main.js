3b36ba20c3387b09fc9aed5295d14738c97410dd
mossa <martin.ossa.8@gmail.com>
2023-02-15 16:06:38 +0100

    Add style display functions

--- main.js
+++ main.js
@@ -1,14 +1,42 @@
 /* eslint-disable class-methods-use-this */
 /* eslint-disable no-unused-vars */
+import { DateTime } from './modules/luxon.js';
 import Book from './modules/Book.js';
 
+const booksList = document.getElementById('booksList');
+const newBook = document.getElementById('newBook');
+const contactInfo = document.getElementById('contactInfo');
+const recordSec = document.querySelector('.record-section');
 const addForm = document.querySelector('.form');
+const contactSec = document.querySelector('.contact');
 const showBooks = document.querySelector('.record');
 const Title = document.querySelector('.title');
 const Author = document.querySelector('.author');
 const btn = document.querySelector('.form button');
+const currentDate = DateTime.local();
+
 
 const bookc = new Book();
+
+
+booksList.addEventListener('click', () => {
+  window.location.reload();
+  recordSec.style.display = 'block';
+  addForm.style.display = 'none';
+  contactSec.style.display = 'none';
+});
+
+newBook.addEventListener('click', () => {
+  recordSec.style.display = 'none';
+  addForm.style.display = 'block';
+  contactSec.style.display = 'none';
+});
+
+contactInfo.addEventListener('click', () => {
+  recordSec.style.display = 'none';
+  addForm.style.display = 'none';
+  contactSec.style.display = 'block';
+});
 
 btn.addEventListener('click', () => {
   if (Title.value === '' || Author.value === '') {
@@ -21,15 +49,19 @@ btn.addEventListener('click', () => {
     };
     bookc.addBook(bookObj);
   }
-  window.location.reload();
 });
+
 
 window.remove = (id) => {
   bookc.removeBook(id);
 };
 
 // Local storage
+
 window.addEventListener('DOMContentLoaded', () => {
+  recordSec.style.display = 'block';
+  addForm.style.display = 'none';
+  contactSec.style.display = 'none';
   const books = JSON.parse(localStorage.getItem('data')) === null ? [] : JSON.parse(localStorage.getItem('data'));
   books.forEach((i) => {
     showBooks.innerHTML += `
